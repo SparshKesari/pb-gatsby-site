a4816e62598af9f13baa7ac6c4633ec4ae1a3e77
Sparsh Kesari <39815871+SparshKesari@users.noreply.github.com>
2020-11-16 15:04:15 +0530

    sidebar

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import Header from './Header'
+import Navbar from './Navbar'
 import Footer from './Footer'
 
 export default function Layout({children}) {
     return (
         <div>
-            <Header />
+            <Navbar />
             <main>
             {children}
             </main>
